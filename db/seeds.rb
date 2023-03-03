69ccc5196c6cb20a9aecd3e95cdf6c8cfbd63791
Doug Barr <douglas.barr01@gmail.com>
2023-03-03 08:24:29 +0000

    Added routes via resources + built a simple seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning the database..."
+Flat.destroy_all
+
+garden_flat = { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3 }  
+clapham_flat = { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 200, number_of_guests: 5 }  
+huge_house = { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 68, number_of_guests: 1 }  
+studio_pad = { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 101, number_of_guests: 5 }  
+penthouse = { name: 'Sleek penthouse in Chelsea', address: '100 Kings Road London SW! 1DT', description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 300, number_of_guests: 8 }  
+
+[garden_flat, clapham_flat, huge_house, studio_pad, penthouse].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+
+puts "Finished seeding the database..."
